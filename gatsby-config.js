ea94213c46b2b0a3ac24f0f9a7328e7b1f612bb8
JayAlegria <jay.alegria@dotdigitalph.com>
2022-07-19 15:21:03 +0800

    Fix google tag id

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "GA-G-MJFHEJMZYD", // Google Analytics / GA
+          "G-MJFHEJMZYD", // Google Analytics / GA
         ],
         // This object is used for configuration specific to this plugin
         pluginConfig: {
