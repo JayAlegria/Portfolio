84618ee5cfcb7e5be02d4d1f2d8cc083155cb9a8
Jay <jay.wbrides@gmail.com>
2025-05-20 19:09:28 +0800

    update details

--- src/components/about/AboutIntroDetails.tsx
+++ src/components/about/AboutIntroDetails.tsx
@@ -7,12 +7,12 @@ export const AboutIntroDetails: FC = () => {
     <div className="about__intro__details">
       <div className="row">
         <div className="col-lg-6">
-          {AboutMe.slice(0, 4).map((item) => (
+          {AboutMe.slice(0, 3).map((item) => (
             <AboutDetailsItem key={item.title} title={item.title} description={item.description} />
           ))}
         </div>
         <div className="col-lg-6">
-          {AboutMe.slice(5, 9).map((item) => (
+          {AboutMe.slice(4, 9).map((item) => (
             <AboutDetailsItem key={item.title} title={item.title} description={item.description} />
           ))}
         </div>
