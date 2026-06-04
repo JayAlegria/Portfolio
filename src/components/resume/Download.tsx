62a31707d4cba32eefe6998c5f3826336a9846ab
jay <alegriajaylaurence@gmail.com>
2026-06-04 15:25:17 +0800

    Feat: Update resume file

--- src/components/resume/Download.tsx
+++ src/components/resume/Download.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import resume from "./JayAlegria.pdf"
+import resume from "./JayAlegriaResume.pdf"
 export const Download = () => {
     return (
         <a href={resume} target='_blank'>
